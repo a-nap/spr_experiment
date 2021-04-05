45f069d86f58f022a111f75e067e3e2b78bd791e
Aleksandar Dimitrov <git@aleks.bg>
2021-04-05 15:18:36 +0200

    Remove non-self-paced reading mode

--- js_includes/SelfPacedReadingParadigmSentence.js
+++ js_includes/SelfPacedReadingParadigmSentence.js
@@ -21,10 +21,6 @@ define_ibex_controller({
         assert_is_arraylike(this.options.s, "Bad value for 's' option of DashedSentence.");
         this.words = this.options.s;
       }
-      this.mode = dget(this.options, "mode", "self-paced reading");
-      assert(this.mode == "self-paced reading" || this.mode == "speeded acceptability",
-             "Value of 'mode' option for DashedSentence controller must be either " +
-             "'self-paced reading' or 'speeded acceptability'.");
       this.display = dget(this.options, "display", "dashed");
       this.blankText = dget(this.options,"blankText", "\u2014\u2014");// Replace with mdash
       this.wordTime = dget(this.options, "wordTime", this.display == "in place" ? 400 : 300); // Only for speeded accpetability.
@@ -84,29 +80,13 @@ define_ibex_controller({
       this.mainDiv.addClass(this.cssPrefix + "sentence");
 
       this.resultsLines = [];
-      if (this.mode == "self-paced reading") {
-        // Don't want to be allocating arrays in time-critical code.
-        this.sprResults = [];
-        for (var i = 0; i < this.words.length; ++i)
-          this.sprResults[i] = new Array(2);
-      }
+      // Don't want to be allocating arrays in time-critical code.
+      this.sprResults = [];
+      for (var i = 0; i < this.words.length; ++i)
+        this.sprResults[i] = new Array(2);
       this.previousTime = null;
 
-      if (this.display == "in place") {
-        this.wordSpan = $(document.createElement("span")).text(this.blankText);
-        if (conf_centerItems) {
-          this.mainDiv.css('text-align', 'center');
-          this.wordSpan.css('text-align', 'center');
-        }
-        this.mainDiv.append(this.wordSpan);
-
-        this.blankWord = this.blankWord_inplace;
-        this.showWord = this.showWord_inplace;
-      }
-      else { // dashed
-        this.blankWord = this.blankWord_dashed;
-        this.showWord = this.showWord_dashed;
-
+      // preparation of dashed sentence starts here
         this.wordISpans = []; // Inner spans.
         this.wordOSpans = []; // Outer spans.
         this.owsnjq = []; // 'outer word spans no jQuery'.
@@ -136,52 +116,16 @@ define_ibex_controller({
           this.iwsnjq.push(ispan[0]);
           this.owsnjq.push(ospan[0]);
         }
-      }
 
-      if (this.mode == "speeded acceptability") {
-        this.showWord(0);
-        var t = this;
-        function wordTimeout() {
-          t.blankWord(t.currentWord);
-          ++(t.currentWord);
-          if (t.currentWord >= t.stoppingPoint)
-            t.finishedCallback([[["Sentence (or sentence MD5)", t.sentenceDesc]]]);
-          else
-            t.utils.setTimeout(wordPauseTimeout, t.wordPauseTime);
-        }
-        function wordPauseTimeout() {
-          t.showWord(t.currentWord);
-          t.utils.clearTimeout(wordPauseTimeout);
-          t.utils.setTimeout(wordTimeout, t.wordTime);
-        }
-        this.utils.setTimeout(wordTimeout, this.wordTime);
-      }
-      else if (this.mode == "self-paced reading") {
-        var t = this;
-        // Inlining this to minimize function calls in code for updating screen after space is pressed.
-        /*            function goToNext(time) {
-                      t.recordSprResult(time, t.currentWord);
+      // spacing through the sentence starts here
 
-                      if (t.currentWord - 1 >= 0)
-                      t.blankWord(t.currentWord - 1);
-                      if (t.currentWord < t.stoppingPoint)
-                      t.showWord(t.currentWord);
-                      ++(t.currentWord);
-                      if (t.currentWord > t.stoppingPoint) {
-                      t.processSprResults();
-                      t.finishedCallback(t.resultsLines);
-                      }
-
-                      return false;
-                      }*/
+      var t = this;
 
         this.safeBind($(document), 'keydown', function(e) {
           var time = new Date().getTime();
           var code = e.keyCode;
 
           if (code == 32) {
-            // *** goToNext() ***
-            //                    t.recordSprResult(time, t.currentWord);
             var word = t.currentWord;
             if (word > 0 && word <= t.stoppingPoint) {
               var rs = t.sprResults[word-1];
@@ -241,12 +185,12 @@ define_ibex_controller({
             // ***
           });
         }
-      }
+
     },
 
     // Not using JQuery in these two methods just in case it slows things down too much.
     // NOTE: [0] subscript gets DOM object from JQuery selector.
-    blankWord_dashed: function(w) {
+    blankWord: function(w) {
       if (this.currentWord <= this.stoppingPoint) {
         this.owsnjq[w].style.borderColor = this.unshownBorderColor;
         this.iwsnjq[w].style.visibility = "hidden";
@@ -254,34 +198,13 @@ define_ibex_controller({
           this.owsnjq[w].style.borderColor = this.background;
       }
     },
-    showWord_dashed: function(w) {
+    showWord: function(w) {
       if (this.currentWord < this.stoppingPoint) {
         if (this.showAhead || this.showBehind)
           this.owsnjq[w].style.borderColor = this.shownBorderColor;
         this.iwsnjq[w].style.visibility = "visible";
       }
     },
-
-    blankWord_inplace: function (w) {
-      if (this.wordPauseTime > 0 && this.currentWord <= this.stoppingPoint) {
-        this.wordSpan.empty();
-      }
-    },
-    showWord_inplace: function (w) {
-      if (this.currentWord < this.stoppingPoint) {
-        this.wordSpan.text(this.words[this.currentWord]);
-      }
-    },
-
-    // Inlining this now.
-    /*recordSprResult: function(time, word) {
-      if (word > 0 && word < this.stoppingPoint) {
-      var rs = this.sprResults[word-1];
-      rs[0] = time;
-      rs[1] = this.previousTime;
-      }
-      this.previousTime = time;
-      },*/
 
     processSprResults: function () {
       var nonSpaceWords = [];
